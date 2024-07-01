2e919a03c406eba8a66145120b7a8dee878228ce
Левин Александр Евгеньевич <aleksandr.levin@tmk-group.com>
2024-07-01 16:27:44 +0500

    Upload

---
+++ app/views/dashboard.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <div>
+        <div class="dashboard_sidebar">
+            <h3 class="dashboard_logo">PVS</h3>
+            <div class="dashboard_sidebar_user">
+                <img src="" alt="User image">
+                <span>Username_place</span>
+            </div>
+            <div class="dashboard_sidebar_menus">
+                <ul class="dashboard_menu_list">
+                    <li>
+                        <a href=""><i class="fa fa-dashboard"></i>Dashboard</a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa fa-dashboard"></i>Dashboard</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="dashboard_content_container">
+            <div class="dashboard_content_topNav">
+                <a href=""><i class="fa fa-navicon"></i></a>
+                <a href=""><i class="fa fa-power-off"></i>Log-out</a>
+            </div>
+            <div class="dashboard_content">
+                <div class="dashboard_content_main">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
