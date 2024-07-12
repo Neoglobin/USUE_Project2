8d6e654f9cd4d7ada0ede055e53c63c7025df225
Левин Александр Евгеньевич <aleksandr.levin@tmk-group.com>
2024-07-12 15:30:36 +0500

    Work on templates

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -4,40 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>FVS dashboard</title>
+    <link rel="stylesheet" href="../css/dashboard.css">
 </head>
 
 <body>
-    <div>
-        <div class="dashboard_sidebar">
-            <h3 class="dashboard_logo">PVS</h3>
-            <div class="dashboard_sidebar_user">
-                <img src="" alt="User image">
-                <span>Username_place</span>
-            </div>
-            <div class="dashboard_sidebar_menus">
-                <ul class="dashboard_menu_list">
-                    <li>
-                        <a href=""><i class="fa fa-dashboard"></i>Dashboard</a>
-                    </li>
-                    <li>
-                        <a href=""><i class="fa fa-dashboard"></i>Dashboard</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="dashboard_content_container">
-            <div class="dashboard_content_topNav">
-                <a href=""><i class="fa fa-navicon"></i></a>
-                <a href=""><i class="fa fa-power-off"></i>Log-out</a>
-            </div>
-            <div class="dashboard_content">
-                <div class="dashboard_content_main">
+    <!-- Левая панель -->
+    <section class="">
 
-                </div>
-            </div>
-        </div>
-    </div>
+    </section>
 </body>
 
 </html>
