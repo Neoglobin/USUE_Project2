c87af32351478498e650318e949890f678f8e777
Левин Александр Евгеньевич <aleksandr.levin@tmk-group.com>
2024-07-16 17:03:57 +0500

    dashboard_page layout design

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -4,14 +4,80 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FVS dashboard</title>
-    <link rel="stylesheet" href="../css/dashboard.css">
+    <title>PVS</title>
+    <link rel="stylesheet" href="css/dashboard.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <!-- Левая панель -->
-    <section class="">
-
+    <section class="leftPanel">
+        <!-- Динамическая информация пользователя -->
+        <div class="profile">
+            <p class="clock">13:45</p>
+            <img class="panelIcon" src="../views/img/warning_icon.png" alt="error">
+            <div class="profileContent">
+                <img class="userIcon" src="../views/img/user_icon.png" alt="error">
+                <p>Александр</p>
+                <h3>Уведомления</h3>
+            </div>
+        </div>
+        <!-- Область уведомлений -->
+        <div class="notifyArea">
+            <div class="notifyBlock">
+                <div class="notifyUpperString">
+                    <h3>11:26</h3>
+                    <h3>Реализован</h3>
+                    <img src="../views/img/close_icon.png" alt="error">
+                </div>
+                <div class="notifyLowerString">
+                    <p>A-12345</p>
+                    <p>N-34005900008411</p>
+                </div>
+            </div>
+            <div class="notifyBlock">
+                <div class="notifyUpperString">
+                    <h3>11:26</h3>
+                    <h3>Реализован</h3>
+                    <img src="../views/img/close_icon.png" alt="error">
+                </div>
+                <div class="notifyLowerString">
+                    <p>A-12345</p>
+                    <p>N-34005900008411</p>
+                </div>
+            </div>
+            <div class="notifyBlock">
+                <div class="notifyUpperString">
+                    <h3>11:26</h3>
+                    <h3>Реализован</h3>
+                    <img src="../views/img/close_icon.png" alt="error">
+                </div>
+                <div class="notifyLowerString">
+                    <p>A-12345</p>
+                    <p>N-34005900008411</p>
+                </div>
+            </div>
+            <div class="notifyBlock">
+                <div class="notifyUpperString">
+                    <h3>11:26</h3>
+                    <h3>Реализован</h3>
+                    <img src="../views/img/close_icon.png" alt="error">
+                </div>
+                <div class="notifyLowerString">
+                    <p>A-12345</p>
+                    <p>N-34005900008411</p>
+                </div>
+            </div>
+        </div>
+        <!-- Панель кнопок -->
+        <div class="buttonPanel">
+            <img class="logoutImage" src="../views/img/logout_icon.png" alt="error">
+            <img class="addImage" src="../views/img/add_icon.png" alt="error">
+        </div>
     </section>
 </body>
 
