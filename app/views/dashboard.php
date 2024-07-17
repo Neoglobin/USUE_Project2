d4b783dda45aacfd842bbf394f343f979905c900
Левин Александр Евгеньевич <aleksandr.levin@tmk-group.com>
2024-07-17 16:05:38 +0500

    Interface development

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -11,74 +11,186 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anta&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anta&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Saira+Semi+Condensed:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <!-- Левая панель -->
-    <section class="leftPanel">
-        <!-- Динамическая информация пользователя -->
-        <div class="profile">
-            <p class="clock">13:45</p>
-            <img class="panelIcon" src="../views/img/warning_icon.png" alt="error">
-            <div class="profileContent">
-                <img class="userIcon" src="../views/img/user_icon.png" alt="error">
-                <p>Александр</p>
-                <h3>Уведомления</h3>
-            </div>
-        </div>
-        <!-- Область уведомлений -->
-        <div class="notifyArea">
-            <div class="notifyBlock">
-                <div class="notifyUpperString">
-                    <h3>11:26</h3>
-                    <h3>Реализован</h3>
-                    <img src="../views/img/close_icon.png" alt="error">
-                </div>
-                <div class="notifyLowerString">
-                    <p>A-12345</p>
-                    <p>N-34005900008411</p>
+    <div class="container">
+        <section class="leftPanel">
+            <!-- Динамическая информация пользователя -->
+            <div class="profile">
+                <p class="clock">13:45</p>
+                <img class="panelIcon" src="../views/img/warning_icon.png" alt="error">
+                <div class="profileContent">
+                    <img class="userIcon" src="../views/img/user_icon.png" alt="error">
+                    <p>Александр</p>
+                    <h3>Уведомления</h3>
                 </div>
             </div>
-            <div class="notifyBlock">
-                <div class="notifyUpperString">
-                    <h3>11:26</h3>
-                    <h3>Реализован</h3>
-                    <img src="../views/img/close_icon.png" alt="error">
+            <!-- Область уведомлений -->
+            <div class="notifyArea">
+                <div class="notifyBlock">
+                    <div class="notifyUpperString">
+                        <h3>11:26</h3>
+                        <h3>Реализован</h3>
+                        <img src="../views/img/close_icon.png" alt="error">
+                    </div>
+                    <div class="notifyLowerString">
+                        <p>A-12345</p>
+                        <p>N-34005900008411</p>
+                    </div>
                 </div>
-                <div class="notifyLowerString">
-                    <p>A-12345</p>
-                    <p>N-34005900008411</p>
+                <div class="notifyBlock">
+                    <div class="notifyUpperString">
+                        <h3>11:26</h3>
+                        <h3>Реализован</h3>
+                        <img src="../views/img/close_icon.png" alt="error">
+                    </div>
+                    <div class="notifyLowerString">
+                        <p>A-12345</p>
+                        <p>N-34005900008411</p>
+                    </div>
+                </div>
+                <div class="notifyBlock">
+                    <div class="notifyUpperString">
+                        <h3>11:26</h3>
+                        <h3>Реализован</h3>
+                        <img src="../views/img/close_icon.png" alt="error">
+                    </div>
+                    <div class="notifyLowerString">
+                        <p>A-12345</p>
+                        <p>N-34005900008411</p>
+                    </div>
+                </div>
+                <div class="notifyBlock">
+                    <div class="notifyUpperString">
+                        <h3>11:26</h3>
+                        <h3>Реализован</h3>
+                        <img src="../views/img/close_icon.png" alt="error">
+                    </div>
+                    <div class="notifyLowerString">
+                        <p>A-12345</p>
+                        <p>N-34005900008411</p>
+                    </div>
                 </div>
             </div>
-            <div class="notifyBlock">
-                <div class="notifyUpperString">
-                    <h3>11:26</h3>
-                    <h3>Реализован</h3>
-                    <img src="../views/img/close_icon.png" alt="error">
-                </div>
-                <div class="notifyLowerString">
-                    <p>A-12345</p>
-                    <p>N-34005900008411</p>
+            <!-- Панель кнопок -->
+            <div class="buttonPanel">
+                <img class="logoutImage" src="../views/img/logout_icon.png" alt="error">
+                <img class="addImage" src="../views/img/add_icon.png" alt="error">
+            </div>
+        </section>
+
+        <!-- Правая панель -->
+        <section class="rightPanel">
+            <div class="header">
+                <div>
+                    <a href="">PVS</a>
                 </div>
             </div>
-            <div class="notifyBlock">
-                <div class="notifyUpperString">
-                    <h3>11:26</h3>
-                    <h3>Реализован</h3>
-                    <img src="../views/img/close_icon.png" alt="error">
+
+            <div class="mainArea">
+                <!-- Левая часть (область изменений) -->
+                <div class="mainAreaLeft">
+                    <div>
+                        <a href="">
+                            <button class="catalogBtn">
+                                <p>Категории товаров</p>
+                            </button>
+                        </a>
+                    </div>
+                    <div class="changeList">
+                        <h3>Список изменений</h3>
+                        <div class="listing">
+                            <img src="../views/img/back_icon.png" alt="error">
+                            <p>3/5</p>
+                            <img src=" ../views/img/next_icon.png" alt="error">
+                        </div>
+                        <div class="changeBlock">
+                            <div class="changeBlockHeader">
+                                <p>A-12345</p>
+                                <p>N-34005900008411</p>
+                            </div>
+                            <p>Удаление записи</p>
+                            <div class="changeBlockFooter">
+                                <p>Изменения внесены</p>
+                                <div class="changeInfo">
+                                    <p>10.07.2024</p>
+                                    <p>16:35</p>
+                                    <p>Александр</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="changeBlock">
+                            <div class="changeBlockHeader">
+                                <p>A-12345</p>
+                                <p>N-34005900008411</p>
+                            </div>
+                            <p>Удаление записи</p>
+                            <div class="changeBlockFooter">
+                                <p>Изменения внесены</p>
+                                <div class="changeInfo">
+                                    <p>10.07.2024</p>
+                                    <p>16:35</p>
+                                    <p>Александр</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <button class="changeConfirmBtn">
+                                <p>Подтвердить</p>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div class="notifyLowerString">
-                    <p>A-12345</p>
-                    <p>N-34005900008411</p>
+
+                <!-- Правая часть (информационная облать) -->
+                <div class="mainAreaRight">
+                    <h1>Список товаров</h1>
+                    <div class="navbar">
+                        <div class="navpanel">
+                            <div>
+                                <select class="navOptions" name="stage" id="stage">
+                                    <option value="active">
+                                        <p>Активные</p>
+                                    </option>
+                                </select>
+                            </div>
+                            <div>
+                                <button class="navPagging" style="background-color: #BC46E8;">
+                                    <p>1</p>
+                                </button>
+                            </div>
+                            <div>
+                                <button class="navPagging">
+                                    <p>2</p>
+                                </button>
+                            </div>
+                            <div>
+                                <button class="navPagging">
+                                    <p>3</p>
+                                </button>
+                            </div>
+                            <div>
+                                <button class="navPagging">
+                                    <p>4</p>
+                                </button>
+                            </div>
+                            <div>
+                                <button class="navPagging">
+                                    <p>5</p>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Панель кнопок -->
-        <div class="buttonPanel">
-            <img class="logoutImage" src="../views/img/logout_icon.png" alt="error">
-            <img class="addImage" src="../views/img/add_icon.png" alt="error">
-        </div>
-    </section>
+        </section>
+
+    </div>
 </body>
 
 </html>
